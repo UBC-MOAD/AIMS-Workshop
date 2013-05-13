a139729a5c72e9b9fa77b654553f2f074b1a31f8
Doug Latornell <djl@douglatornell.ca>
2013-05-13 21:18:18 +0200

    Fix syntax error.

--- conf.py
+++ conf.py
@@ -17,10 +17,8 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(
-    0, os.path.abspath('dynmodes'),
-    0, os.path.abspath('thermal_wind'),
-)
+sys.path.insert(0, os.path.abspath('dynmodes'))
+sys.path.insert(0, os.path.abspath('thermal_wind'))
 
 # -- General configuration -----------------------------------------------------
 
